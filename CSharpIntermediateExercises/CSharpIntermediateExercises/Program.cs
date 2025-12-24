5dca4b18a9b0b87bbbd829c00783380cd03bd78a
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2025-12-24 11:52:20 +0000

    [ADD] C# Intermediate, finished Stopwatch class

--- CSharpIntermediateExercises/CSharpIntermediateExercises/Program.cs
+++ CSharpIntermediateExercises/CSharpIntermediateExercises/Program.cs
@@ -10,6 +10,29 @@ namespace CSharpIntermediateExercises
     {
         static void Main(string[] args)
         {
+            Stopwatch sw = new Stopwatch();
+
+            bool ongoing = true;
+            while (ongoing) {
+                Console.WriteLine("Please type 'start' to start the Stopwatch and 'stop' to end it or 'quit' to end the program \n");
+                string input = Console.ReadLine();
+
+                if (input.Equals("start"))
+                {
+                    if (sw.Started == true)
+                    {
+                        throw new InvalidOperationException("The stopwatch has already been started.");
+                    }
+                    sw.Start();
+                } else if (input.Equals("stop")) {
+                    sw.Stop();
+                } else if (input.Equals("quit"))
+                {
+                    ongoing = false;
+                }
+            }
+
+            
         }
     }
 }
--- CSharpIntermediateExercises/CSharpIntermediateExercises/Stopwatch.cs
+++ CSharpIntermediateExercises/CSharpIntermediateExercises/Stopwatch.cs
@@ -8,5 +8,30 @@ namespace CSharpIntermediateExercises
 {
     class Stopwatch
     {
+        // User starts StopWatch
+        // Get time now
+
+        // User stops StopWatch
+        // Get end time, and duration between the two
+
+        private DateTime _start;
+        private DateTime _end;
+        private TimeSpan _duration;
+        public bool Started { get; set; } // Property
+
+
+        public void Start() {
+            _start = DateTime.Now;
+            Started = true;
+            Console.WriteLine("Stopwatch started \n");
+        }
+
+        public void Stop() {
+            _end = DateTime.Now;
+            _duration = _end - _start;
+            Started = false;
+            Console.WriteLine(_duration);
+            Console.WriteLine("Stopwatch stopped \n");
+        }
     }
 }
