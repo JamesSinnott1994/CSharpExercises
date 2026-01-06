654f55e12e63f189ae38fc1336090168a6e28a90
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2026-01-06 12:08:33 +0000

    [ADD] C# Intermediate - Stack start

---
+++ CSharpIntermediateExercises/CSharpIntermediateExercises/Stack.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpIntermediateExercises
+{
+    class Stack
+    {
+        public void Push(object obj) { 
+        
+        }
+
+        public object Pop() {
+            return null;
+        }
+
+        public void Clear() { 
+        
+        }
+    }
+}
