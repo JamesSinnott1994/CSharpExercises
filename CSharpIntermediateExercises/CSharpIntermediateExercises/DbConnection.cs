db97c5376a3f487487b1685cc693de37e3852382
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2026-01-15 16:19:11 +0000

    [ADD] C# Intermediate - Desgin Database Connection - DbConnection abstract class added

---
+++ CSharpIntermediateExercises/CSharpIntermediateExercises/DbConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpIntermediateExercises
+{
+    abstract class DbConnection
+    {
+        private String ConnectionString { get; set; }
+        private TimeSpan Timeout { get; set; }
+
+        public DbConnection(String connectionString) {
+
+            if (connectionString == null || connectionString.Equals(""))
+                throw new InvalidOperationException("Connection string must be valid");
+
+            this.ConnectionString = connectionString;
+        }
+
+        public abstract void Open();
+        public abstract void Closed();
+    }
+}
